9abc6aa75ad90c299cd6612e444453decaada470
Jyotiswar Chakraborty <jyotiswarchakrabortyabc18@gmail.com>
2026-04-27 12:16:49 +0530

    pushed

---
+++ Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+     
+     int i = 0;
+     int j = 1;
+     while(j<nums.size()){
+        if(nums[i] != nums[j]){
+            nums[i+1] = nums[j];
+            i++;
+            j++;
+        }else{
+            j++;
+        }
+     }
+     return i+1;
+    
+    }
+};
