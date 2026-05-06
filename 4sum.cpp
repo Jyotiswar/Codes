ba17bbd50eaf0a724589191386fa0b4366af6fa5
Jyotiswar Chakraborty <jyotiswarchakrabortyabc18@gmail.com>
2026-05-06 10:36:24 +0530

    commited

---
+++ 4sum.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+class solution{
+
+    vector<vector<int>>FourSum(int target, vector<int>&nums){
+        
+        sort(nums.begin(),nums.end());
+
+        vector<vector<int>> result;
+        
+        int n = nums.size();
+
+        for(int i = 0 ; i < n ; i++){
+            
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+                
+                for(int j = i+1 ; j < n ; j++){
+                    
+                    if(j > i+1 && nums[j] == nums[j-1]) continue;
+
+                    int left = j+1;
+                    int right = n-1;
+
+                    int sum = nums[i]+nums[j]+nums[left]+nums[right];
+
+                    while(left < right){
+
+                        if (sum == target){
+                            result.push_back({nums[i],nums[j],nums[left],nums[right]});
+                            left++;right--;
+
+                            while(left < right && nums[left] == nums[left-1])left++;
+                            while(left < right && nums[right] == nums[right+1])right--;
+                        }else if(sum < target){
+                            left++;
+                        }else{
+                            right--;
+                        }
+
+                    }
+                }
+        }
+        return result;
+    }
+};
