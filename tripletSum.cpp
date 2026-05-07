82f71453dc86b069062e2d53ce202994d3d32f92
Jyotiswar Chakraborty <jyotiswarchakrabortyabc18@gmail.com>
2026-05-07 21:27:03 +0530

    commited

--- tripletSum.cpp
+++ tripletSum.cpp
@@ -25,8 +25,8 @@ public:
                     result.push_back({nums[i],nums[left],nums[right]});
                     left++,right--;
                 
-                while(left < right && nums[left] == nums[left-1]) left++;
-                while(left < right && nums[right] == nums[right+1]) right--;
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
                 }else if(s < sum){
                     left++;
                 }else{
