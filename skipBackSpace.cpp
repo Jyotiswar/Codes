50e94ab832e6084dfee925af9d4e4ef61d94fa04
Jyotiswar Chakraborty <jyotiswarchakrabortyabc18@gmail.com>
2026-05-07 21:22:10 +0530

    commited

---
+++ skipBackSpace.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+class solution{
+    
+    bool skipIfSpace(string s, string t){
+
+        int skipS = 0, skipT = 0;
+        int i = s.size()-1, j=t.size()-1;
+        
+        
+    } 
+};
